8443128f041cebb02e6ed1d2ff9c816e5af1a938
sandaljack <425862717@qq.com>
2018-05-27 22:26:28 +0800

    数据动态填充详情页

--- ReaderMovie/pages/posts/post-detail/post-detail.js
+++ ReaderMovie/pages/posts/post-detail/post-detail.js
@@ -1,3 +1,16 @@
+var postsData = require("../../../data/posts-data.js");
+
 Page({
-  
+  onLoad:function(option){
+    var postId = option.id;
+    var postData = postsData.postList[postId];
+    //如果在onLoad方法中，不是异步去执行一个数据绑定
+    // 则不需要使用this.setData方法
+    //只需要对this.data赋值即可实现数据绑定
+    // this.data.postData = postData;
+    // console.log(postData);
+    this.setData({
+      postData:postData
+    })
+  }
 })
