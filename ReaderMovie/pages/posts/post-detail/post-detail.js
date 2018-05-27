47b02723dbc6772cb06da089eb15dd9c648874de
sandaljack <425862717@qq.com>
2018-05-28 00:26:45 +0800

    使用缓存实现文章收藏功能

--- ReaderMovie/pages/posts/post-detail/post-detail.js
+++ ReaderMovie/pages/posts/post-detail/post-detail.js
@@ -1,8 +1,12 @@
 var postsData = require("../../../data/posts-data.js");
 
 Page({
+  data:{
+
+  },
   onLoad:function(option){
     var postId = option.id;
+    this.data.currentPostId = postId;
     var postData = postsData.postList[postId];
     //如果在onLoad方法中，不是异步去执行一个数据绑定
     // 则不需要使用this.setData方法
@@ -13,19 +17,31 @@ Page({
       postData:postData
     })
 
-    // 设置缓存
-    wx.setStorageSync('key', {game:'LOL',developer:'风暴英雄'})
+    // 读取缓存的收藏值
+    var postsCollected = wx.getStorageSync('posts_collected');
+    if (postsCollected) {
+      var postCollected = postsCollected[postId];
+       this.setData({
+         collected:postCollected
+       })
+    } else {
+      var postsCollected = {}
+      postsCollected[postId] = false;
+      wx.setStorageSync('posts_collected', postsCollected);
+    }
+    
   },
-  onCollectionTap: function (event) {
-    // 获得缓存
-    var game = wx.getStorageSync('key');
-    console.log(game);
+  onCollectTap:function(event){
+    var postsCollected = wx.getStorageSync('posts_collected');
+    var postCollected = postsCollected[this.data.currentPostId];
+    // 收藏变成未收藏，未收藏变成收藏
+    postCollected = !postCollected;
+    postsCollected[this.data.currentPostId] = postCollected;
+    // 更新文章是否的缓存值
+    wx.setStorageSync('posts_collected', postsCollected);
+    // 更新数据绑定变量，从而实现切换图片
+    this.setData({
+      collected:postCollected
+    })
   },
-  onShareTap:function(event) {
-    // 清除指定缓存
-    //缓存的上限最大不能超过10MB
-    wx.removeStorageSync('key');
-    // 清除所有缓存
-    // wx.clearStorageSync();
-  }
 })
