40b07a04ad83fb1fbcdf5d6999b8c333f3d01642
sandaljack <425862717@qq.com>
2018-05-27 19:27:08 +0800

    小程序事件机制

--- ReaderMovie/pages/welcome/welcome.js
+++ ReaderMovie/pages/welcome/welcome.js
@@ -1,3 +1,21 @@
 Page({
-  
+  onTap:function(){
+    // 有子集，原来的被隐藏
+    // wx.navigateTo({
+    //   url: '../posts/post',
+    // });
+
+    // 直接跳转，不能跳回
+    wx.redirectTo({
+      url: '../posts/post',
+    });
+  },
+  onUnload:function(){
+
+  },
+
+  onHide:function(){
+
+  }
+
 })
