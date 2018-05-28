75f21690aa4862f8d94531f3dd27aec535a415e4
sandaljack <425862717@qq.com>
2018-05-28 20:10:35 +0800

    加入tap选项卡

--- ReaderMovie/pages/welcome/welcome.js
+++ ReaderMovie/pages/welcome/welcome.js
@@ -1,14 +1,14 @@
 Page({
   onTap:function(){
     // 有子集，原来的被隐藏
-    // wx.navigateTo({
-    //   url: '../posts/post',
-    // });
-
-    // 直接跳转，不能跳回
-    wx.redirectTo({
+    wx.navigateTo({
       url: '../posts/post',
     });
+   
+    // 直接跳转，不能跳回
+    // wx.redirectTo({
+    //   url: '../posts/post',
+    // });
   },
 
 })
